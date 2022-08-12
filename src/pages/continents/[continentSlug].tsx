e4056da43d32783b12424bbfae104de1a512869e
Régis Oliveira <regisoliveiramelo@gmail.com>
2022-08-12 15:08:47 -0300

    adição de titulo para a pagina

--- src/pages/continents/[continentSlug].tsx
+++ src/pages/continents/[continentSlug].tsx
@@ -3,6 +3,7 @@ import { GetServerSideProps } from 'next'
 import { CitiesGalery } from '../../components/CitiesGalery'
 import { ContinentCover } from '../../components/Banners/ContinentBanner'
 import { Information } from '../../components/Information'
+import Head from 'next/head'
 
 export type Top100CityType = {
   city: string;
@@ -36,6 +37,9 @@ export default function Continents({cities, continentSlug, continentsSumary}: Co
   
   return (
     <Box as="main" w="100%">
+      <Head>
+        <title>{cities[0].continentName}</title>
+      </Head>
       <ContinentCover continentName={cities[0].continentName} continentSlug={continentSlug} />
       <Information continentSumary={continentsSumary[continentSlug]} />
       <CitiesGalery cities={cities.reverse()} continentSlug={continentSlug} />
